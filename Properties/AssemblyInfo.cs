e2a3c487850f8af8e0b624e5a82277b7ed2764af
niketashesh <niket.ashesh@gmail.com>
2014-11-18 16:43:36 -0800

    updated to .net 4.5 and tested on Sitecore 7.5

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,10 +2,10 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("MultiSites Manager")]
 [assembly: AssemblyDescription("MultiSites Manager")]
-[assembly: AssemblyConfiguration("SC6.5")]
+[assembly: AssemblyConfiguration("SC7.5")]
 [assembly: AssemblyCompany("Sitecore Corporation")]
 [assembly: AssemblyProduct("Sitecore.NET")]
-[assembly: AssemblyCopyright("© Sitecore Corporation 2007-2012")]
+[assembly: AssemblyCopyright("© Sitecore Corporation 2007-2014")]
 [assembly: AssemblyTrademark("Sitecore")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyVersion("3.1.2.1")]
